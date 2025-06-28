9f1787d6353356a0d6337360f02086200866ff10
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-06-28 12:57:04 +0600

    inngest updated

--- inngest/server.js
+++ inngest/server.js
@@ -7,7 +7,7 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 // Inngest Function to save user data to a database 
 const syncUserCreation = inngest.createFunction(
     { id: 'sync-user-from-clerk' },
-    { event: 'clerk/user-created' },
+    { event: 'clerk/user.created' },
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
@@ -21,9 +21,9 @@ const syncUserCreation = inngest.createFunction(
 )
 
 // Inngest Function to delete user from database
-const syncUserDelete = inngest.createFunction(
+const syncUserDeletion = inngest.createFunction(
     { id: 'delete-user-with-clerk' },
-    { event: 'clerk/user-deleted' },
+    { event: 'clerk/user.deleted' },
     async ({ event }) => {
         const { id } = event.data
         await User.findByIdAndDelete(id)
@@ -33,7 +33,7 @@ const syncUserDelete = inngest.createFunction(
 // Inngest Function to update user from database
 const syncUserUpdation = inngest.createFunction(
     { id: 'update-user-from-clerk' },
-    { event: 'clerk/user-updated' },
+    { event: 'clerk/user.updated' },
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
@@ -48,6 +48,6 @@ const syncUserUpdation = inngest.createFunction(
 
 export const functions = [
     syncUserCreation, 
-    syncUserDelete,
+    syncUserDeletion,
     syncUserUpdation
 ];
