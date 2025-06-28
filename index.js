075c5e4a247c287777a7092db6c143c6ceea422e
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-06-28 19:43:22 +0600

    booking routes added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/server.js";
 import showRouter from "./routes/showRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 
 const app = express();
 const port = 3000;
@@ -23,7 +24,10 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/inngest', serve({ client: inngest, functions }))
+// Show Routes
 app.use('/api/show', showRouter)
+// Booking Routes
+app.use('/api/booking', bookingRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
