967edb401cf50a8a01a55bcff7dfffc4a59a9945
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-06-28 12:36:59 +0600

    inngest and clerk connect to database

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config';
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/server.js";
 
 const app = express();
 const port = 3000;
 
+await connectDB()
+
 // Middleware
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 // API Routes
 app.get('/', (req, res) => {
     res.send("Quick Server is running now!")
 })
+
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
